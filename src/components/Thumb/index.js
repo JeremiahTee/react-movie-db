fa345bed2590dd0aa859705f6222219e5da43112
Jeremiah <jeremiahteee@gmail.com>
2022-01-29 22:02:58 -0500

    routing

--- src/components/Thumb/index.js
+++ src/components/Thumb/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { Image } from './Thumb.styles';
 
@@ -6,7 +7,13 @@ const Thumb = (
   { image, movieId, clickable } //explicit return since will only return jsx
 ) => (
   <div>
-    <Image src={image} alt='movie-thumb' />
+    {clickable ? (
+      <Link to={`/${movieId}`}>
+        <Image src={image} alt='movie-thumb' />
+      </Link>
+    ) : (
+      <Image src={image} alt='movie-thumb' />
+    )}
   </div>
 );
 
